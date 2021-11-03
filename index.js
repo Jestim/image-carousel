64713166f417371f1462143a29e4d878cb0cd4ca
Jestim <jesper.os.thiman@gmail.com>
2021-11-03 21:23:17 +0100

    Circles added but no highlight

--- index.js
+++ index.js
@@ -1,25 +1,52 @@
-const frames = document.querySelectorAll('.frame');
-const container = document.querySelector('.container');
-const nextButton = document.getElementById('next');
-const previousButton = document.getElementById('previous');
+// DOM elements
+const frameElements = document.querySelectorAll('.frame');
+const containerElement = document.querySelector('.container');
+const nextButtonElement = document.getElementById('next');
+const previousButtonElement = document.getElementById('previous');
+const circleElements = document.querySelectorAll('.circle');
 
+// Adding event listeners
+nextButtonElement.addEventListener('click', renderNextImage);
+previousButtonElement.addEventListener('click', renderPreviousImage);
+circleElements.forEach(circle => {
+    circle.addEventListener('click', (e) => {
+        displayImage(e.target.id);
+    })
+});
+
+// creating an object for each image and storing in an array
+function createFrameObject(id, element) {
+    return {
+        id,
+        element
+    }
+}
+
+let frames = [];
+let IdCounter = 1;
+frameElements.forEach(frame => {
+    frames.push(createFrameObject(IdCounter, frame));
+    IdCounter++;
+});
+
+// Keep track of the images
 let currentImage;
 let nextImage;
 let previousImage;
 
-function getImageOrder() {
-    frames.forEach(frame => {
+function updateImageOrder() {
+    frameElements.forEach(frame => {
         if (!frame.classList.contains('invisible')) {
             currentImage = frame;
 
             if (!frame.nextElementSibling) {
-                nextImage = container.firstElementChild;
+                nextImage = containerElement.firstElementChild;
             } else {
                 nextImage = frame.nextElementSibling;
             }
 
             if (!frame.previousElementSibling) {
-                previousImage = container.lastElementChild;
+                previousImage = containerElement.lastElementChild;
             } else {
                 previousImage = frame.previousElementSibling;
             }
@@ -27,20 +54,34 @@ function getImageOrder() {
     });
 }
 
-getImageOrder();
+updateImageOrder();
 
 function renderNextImage() {
     currentImage.classList.add('invisible');
     nextImage.classList.remove('invisible');
-    getImageOrder();
+    updateImageOrder();
 }
-
-nextButton.addEventListener('click', renderNextImage);
 
 function renderPreviousImage() {
     currentImage.classList.add('invisible');
     previousImage.classList.remove('invisible');
-    getImageOrder();
+    updateImageOrder();
 }
 
-previousButton.addEventListener('click', renderPreviousImage);
+function updateImage(element) {
+    frameElements.forEach(frame => {
+        if (!frame.classList.contains('invisible')) {
+            frame.classList.add('invisible')
+        }
+    });
+    element.classList.remove('invisible');
+    updateImageOrder()
+}
+
+function displayImage(id) {
+    frames.forEach(frame => {
+        if (frame.id == id) {
+            updateImage(frame.element);
+        }
+    });
+}
